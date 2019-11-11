850532191d34ec6e937f499b95efc90b58614a72
HeZiFeng <tenny_0119@163.com>
2019-11-11 15:49:14 +0800

    tvOS demo

--- tvOS_demo/AppDelegate.swift
+++ tvOS_demo/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let screen = UIScreen.main.bounds        //获得设备尺寸
+        window = UIWindow.init(frame: screen) //给“输出窗口“实例化并设置frame
+        let vc = ViewController()
+        window?.rootViewController = vc//将“输出窗口”的根视图设置为导航栏
+        window?.makeKeyAndVisible()        
+        
         return true
     }
 
